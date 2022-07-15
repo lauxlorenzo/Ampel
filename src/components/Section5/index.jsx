da108fbad103ed5af3a4c924513f7aaf62b21f63
lauxlorenzo <lorenzolaux05@gmail.com>
2022-07-15 09:21:20 -0300

    Version 1.2.1

--- src/components/Section5/index.jsx
+++ src/components/Section5/index.jsx
@@ -6,17 +6,21 @@ import Bg3 from '../../assets/Imagens/Desktop/Bg-3.jpg'
 function Section5() {
   return (
     <div className='Section5'>
-      <h1>
-        Melhorar a vida das pessoas,<br/>
-        dos negócios e o ambiente.
-      </h1>
+      <div className='section5-title'>
+        <h1>
+          Melhorar a vida das pessoas,
+          dos negócios e o ambiente.
+        </h1>
+      </div>
 
-      <p>
-        Nosso compromisso é com o aumento,<br/>
-        desenvolvimento e pulverização da geração<br/>
-        de energia limpa e com a segurança, autonomia<br/>
-        e independência energética.
-      </p>
+      <div className='section5-description'>
+        <p>
+          Nosso compromisso é com o aumento,
+          desenvolvimento e pulverização da geração
+          de energia limpa e com a segurança, autonomia
+          e independência energética.
+        </p>
+      </div>
     </div>
   )
 }
